2b5cccfd1e156bd9497caf05f402f6619929e637
Josie Leye <jiley1@student.monash.edu>
2021-05-20 17:28:18 +1000

    Extract bitmap preprocessing into separate class to separate testing

--- app/src/main/java/com/example/unblind/BitmapProcessor.java
+++ app/src/main/java/com/example/unblind/BitmapProcessor.java
@@ -1,2 +1,18 @@
-package com.example.unblind;public class BitmapProcesseor {
+package com.example.unblind;
+
+import android.graphics.Bitmap;
+
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+public class BitmapProcessor {
+    float[] mean = {0.485f, 0.456f, 0.406f};
+    float[] std = {0.229f, 0.224f, 0.225f};
+
+    public Tensor preprocess(Bitmap bitmap, int size){
+
+        bitmap = Bitmap.createScaledBitmap(bitmap,size,size,false);
+        return TensorImageUtils.bitmapToFloat32Tensor(bitmap,this.mean,this.std);
+
+    }
 }
