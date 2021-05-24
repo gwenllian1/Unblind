ad17e5b6db6556d618788dd86b1217d3d2bcb3b7
Edwin Lee <elee0016@student.monash.edu>
2021-05-24 19:31:45 +1000

    Database unit testing
    The database runs but the unit test failed to call the database service itself.

--- app/src/test/java/com/example/unblind/IntegrationUnitTest.java
+++ app/src/test/java/com/example/unblind/IntegrationUnitTest.java
@@ -1,15 +1,27 @@
 package com.example.unblind;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+//import androidx.test.runner.AndroidJUnit4;
 
 import com.example.unblind.DatabaseService;
 
-
+//@RunWith(AndroidJUnit4::class)
 public class IntegrationUnitTest {
     String inputPref = "preferenceName1";
     String inputKey = "InputKey1";
     String inputLabel = "InputLabel1";
+    private Context ApplicationProvider;
+    IBinder service;
+
+//    @Rule
+//    public final ServiceTestRule serviceRule = new ServiceTestRule();
 
 
     @Test
@@ -20,7 +32,14 @@ public class IntegrationUnitTest {
     @Test
     public void DatabaseTest1(){
         //testing if the database is working
-        DatabaseService TestDB = new DatabaseService();
+//        DatabaseService TestDB = new DatabaseService();
+
+//        Intent serviceIntent = new Intent(ApplicationProvider.getApplicationContext(), DatabaseService.class);
+
+//        IBinder binder = serviceRule.bindService(serviceIntent);
+        DatabaseService.LocalBinder binder = (DatabaseService.LocalBinder) service;
+//        DatabaseService TestDB = ((DatabaseService.LocalBinder) service).getService();
+        DatabaseService TestDB = binder.getService();
 
         TestDB.setSharedData(inputPref, inputKey, inputLabel);
         String outputResult = TestDB.getSharedData(inputPref, inputKey);
