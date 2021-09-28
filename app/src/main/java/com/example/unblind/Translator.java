fb22959dab2aac79a6eb8e621e594dd16fe0fdc3
Duc <mvuu0003@student.monash.edu>
2021-09-28 19:10:00 +1000

    Updated the newest dictionary for the building model from team 3, refactor the code in translator and added javadoc comments

--- app/src/main/java/com/example/unblind/Translator.java
+++ app/src/main/java/com/example/unblind/Translator.java
@@ -12,14 +12,17 @@ import java.util.ArrayList;
 public class Translator {
 
     ArrayList<String[]> vocabulary = new ArrayList<>();
-
     public Translator(Context context) {
         setup(context);
     }
 
+    /**
+     * This is a function that will be called inside the constructor to load and store data into our vocabulary.
+     * @param context: the class that contains useful information about the runtime environment.
+     */
     private void setup(Context context) {
-
-        String content = getStringFromFile("vocab_data.txt", context);      // get all the content of the vocab file.
+        String fileName = "vocab_data.txt";
+        String content = getStringFromFile(fileName, context);      // get all the content of the vocab file.
         String[] items = content.split(";");        // split the data to build vocab data structure
         for (String item : items) {
             String[] itemLanguage = item.split(",");
@@ -27,40 +30,59 @@ public class Translator {
         }
     }
 
-    public static String convertStreamToString(InputStream is) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));  // read data from txt file
-        StringBuilder sb = new StringBuilder();
+    /**
+     * This is a function that will convert a stream input into a string.
+     * @param inputStream : the stream of input as bytes
+     * @return : a string for given input stream
+     * @throws Exception
+     */
+    public static String convertStreamToString(InputStream inputStream) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));      // read data from txt file
+        StringBuilder stringBuilder = new StringBuilder();      // initialize a string builder
         String line = null;
         while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
+            stringBuilder.append(line).append("\n");    // append each line from the file into the created stream builder.
         }
-        reader.close();
-        return sb.toString();
+        reader.close();     // close the reader from the input file
+        return stringBuilder.toString();    // convert string builder to string and return.
     }
 
+
+    /**
+     * This function will get the string data from a given file
+     * @param filePath : the path to file( filename) for files in assets folder.
+     * @param context : the runtime information bundle.
+     * @return: string for the data from the file.
+     */
     public static String getStringFromFile(String filePath, Context context) {
-        InputStream fl = null;
+        InputStream inputStream = null;     // initialize the input data stream
         try {
-            fl = context.getAssets().open(filePath);
+            inputStream = context.getAssets().open(filePath);   // set it to the passed in filename
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String ret = null;
+        String returnString = null;
         try {
-            ret = convertStreamToString(fl);
+            returnString = convertStreamToString(inputStream);  // read the data from stream to return
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //Make sure you close all streams.
+
         try {
-            assert fl != null;
-            fl.close();
+            assert inputStream != null;
+            inputStream.close();    // close the stream to avoid any memory leaks.
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return ret;
+        return returnString;
     }
 
+    /**
+     * This function will be used inside the TTS engine to find the text translation for given label
+     * @param englishLabel: the original label, which will be used to find the translation.
+     * @param desiredLanguageCode: the index of that string (0-1-2) in the language item String array.
+     * @return the string represents the label of the word ( can be in different languages)
+     */
     public String searchMatchingLanguageLabel(String englishLabel, int desiredLanguageCode) {
         for (String[] item : vocabulary) {  // find the label inside the dictionary
             if (item[0].toLowerCase().trim().equals(englishLabel.toLowerCase().trim()) && item.length == 3) {
