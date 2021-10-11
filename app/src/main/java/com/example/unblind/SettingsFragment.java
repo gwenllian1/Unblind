63057b4c34fcfc5084a4defd4e5bc4ec39a11da0
iyeo0002 <iyeo0002@student.monash.edu>
2021-10-11 13:49:01 +1100

    Updated javadoc and comments for SettingsFragment
    Sorted out the javadoc and comments for SettingsFragment.java

--- app/src/main/java/com/example/unblind/SettingsFragment.java
+++ app/src/main/java/com/example/unblind/SettingsFragment.java
@@ -22,49 +22,81 @@ import java.util.Locale;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+
+    /**
+     * Returns a boolean value of whether the Unblind Service is running or not.
+     * Used as a helper function for a condition check in serviceCheck.
+     *
+     * @param  context  the current context
+     * @param  accessibilityServiceClass the unblind service class
+     * @return      a boolean value (true/false)
+     */
     public boolean isAccessServiceEnabled(Context context, Class accessibilityServiceClass)
     {
+        // get currently running accessibility service, will be null if none are running
         String prefString = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
 
+        // return true if the Unblind service is running, and false if it isn't
         return prefString!= null && prefString.contains(context.getPackageName() + "/" + accessibilityServiceClass.getName());
     }
 
+
+    /**
+     * Sets the Service Status text to On or Off in the UI, depending on whether the service is
+     * running at the time.
+     *
+     * @param  button  the UI button whose text will be edited.
+     */
     public void serviceCheck (Preference button) {
+        // if statement to check if the Unblind service is currently running
         if (isAccessServiceEnabled(getContext(), UnblindAccessibilityService.class)) {
-            button.setTitle("Service Status: ON");
+            button.setTitle("Service Status: ON"); // set service status button text to ON
         } else {
-            button.setTitle("Service Status: OFF");
+            button.setTitle("Service Status: OFF"); // set service status button text to OFF
         }
     }
 
+
+    /**
+     * Listener function for the preference settings. Will trigger activities based on what
+     * button is pressed in the UI.
+     *
+     * @param  savedInstanceState  the state of the app
+     * @param  rootKey the preference key of the PreferenceScreen to use as the root of the preference hierarchy, or null to use the root PreferenceScreen.
+     * @return      true on button press
+     */
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
         Preference button = findPreference(getString(R.string.service_button));
         Preference ttsOptions = findPreference(getString(R.string.ttsOptions_button));
         Preference notificationButton = findPreference(getString(R.string.activate_notification_key));
+
+        // listener for the Service Status button
         button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //Code below for when Settings Preference is clicked
+                // Send user to Accessibility Settings when Service Status is clicked
                 startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
                 return true;
             }
         });
 
+        // listener for the Language button
         ttsOptions.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //Code below for when Language is clicked
+                // Send user to text-to-speech settings when Language is clicked
                 startActivity(new Intent("com.android.settings.TTS_SETTINGS"));
                 return true;
             }
         });
 
+        // listener for the Notification button
         notificationButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //Code below for when Notification is clicked
+                //Reactivate Notification when the Notification button is clicked
                 getActivity().stopService(new Intent(getActivity(), ModelService.class));
                 getActivity().startService(new Intent(getActivity(),ModelService.class));
                 return true;
@@ -73,24 +105,34 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     }
 
+
+    /**
+     * Utilises the android onResume method to update the service status text when the service
+     * is enabled/disabled.
+     *
+     */
     @Override
     public void onResume() {
         super.onResume();
 
         // Service Status Display (ON/OFF)
         Preference button = findPreference(getString(R.string.service_button));
-
         serviceCheck(button);
 
     }
 
+
+    /**
+     * Utilises the android onStart method to update the service status text when the service
+     * is enabled/disabled.
+     *
+     */
     @Override
     public void onStart() {
         super.onStart();
 
         // Service Status Display (ON/OFF)
         Preference button = findPreference(getString(R.string.service_button));
-
         serviceCheck(button);
     }
 
