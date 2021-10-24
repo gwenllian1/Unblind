248722804843f364b051b9e82a567cac67559b09
Ruben Barylak <rbar34@student.monash.edu>
2021-10-24 18:28:01 +1100

    add GPLv3

--- app/src/main/java/com/example/unblind/MainActivity.java
+++ app/src/main/java/com/example/unblind/MainActivity.java
@@ -1,3 +1,20 @@
+//        Copyright 2021 Project 3
+
+//        This file is part of UnBlind.
+//
+//        UnBlind is free software: you can redistribute it and/or modify
+//        it under the terms of the GNU General Public License as published by
+//        the Free Software Foundation, either version 3 of the License, or
+//        (at your option) any later version.
+//
+//        UnBlind is distributed in the hope that it will be useful,
+//        but WITHOUT ANY WARRANTY; without even the implied warranty of
+//        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//        GNU General Public License for more details.
+//
+//        You should have received a copy of the GNU General Public License
+//        along with UnBlind.  If not, see <https://www.gnu.org/licenses/>.
+
 package com.example.unblind;
 
 import android.os.Bundle;
